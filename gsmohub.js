74c851ce2acf7f93b13211e6ee02843fb6ab16d9
Peter Carrero <peter.carrero@gmail.com>
2014-05-14 16:19:51 -0500

    Create squeleton extension

---
+++ gsmohub.js
@@ -0,0 +1,11 @@
+jQuery.each(
+  $('blockquote:contains("GSMO#")'),
+  function(i,val) {
+    $(val).html($(val).html().replace(
+      /(gsmo#)([\d]*)/i,
+      function(a,b,c){
+        return '<a href="http://gsmo.hidglobal.com/global-services/node/'+c+'" target="_blank">'+b+c+'</a>';
+      }
+    ));
+  }
+);
